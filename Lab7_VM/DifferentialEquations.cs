d00864a59505f7a28bc0bc18ab1ee693c001e00b
Алексей Гардер <aleksey_garder@outlook.com>
2018-04-29 23:07:21 +0700

    Добавьте файлы проекта.

---
+++ Lab7_VM/DifferentialEquations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7_VM
+{
+    static class DifferentialEquations
+    {
+        public delegate double InputFunction(double x, double y);
+
+        public static IEnumerable<(double x, double y)> GetPureEuler(InputFunction f, double x0, double y0, double h, int count)
+        {
+            for (var i = 0; i < count; i++)
+            {
+                yield return (x0, y0);
+                y0 += h * f(x0, y0);
+                x0 += h;
+            }
+        }
+
+        public static IEnumerable<(double x, double y)> GetModifiedEuler(InputFunction f, double x0, double y0, double h, int count)
+        {
+            for(var i = 0; i < count; i++)
+            {
+                yield return (x0, y0);
+                y0 += h * f(x0 + h / 2, y0 + h / 2 * f(x0, y0));
+                x0 += h / 2;
+            }
+        }
+    }
+}
---
+++ Lab7_VM/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab7_VM
+{
+    public partial class Form1 : Form
+    {
+        double Fuc(double x, double y) => 0.1 * y;
+
+        public Form1()
+        {
+            foreach (var (x, y) in DifferentialEquations.GetPureEuler(Fuc, 0, 1000, 1.0/12.0, 60))
+            {
+                Console.WriteLine(x.ToString() + ' ' + y.ToString());
+            }
+            InitializeComponent();
+        }
+    }
+}
